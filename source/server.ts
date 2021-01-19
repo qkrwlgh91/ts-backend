9d1bd4476898ea05107f1e22434c43fd53db1003
qkrwlgh91 <qkrwlgh91@gmail.com>
2021-01-20 00:09:41 +0900

    server.ts - Rule of our API

--- source/server.ts
+++ source/server.ts
@@ -21,3 +21,13 @@ router.use((req, res, next) => {
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
+/** Rule of our API */
+router.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST PUT');
+        return res.status(200).json({});
+    }
+});
